97864e11cfe2aad6e7a24ec54c82bec1f7bb061b
Ky Northstar <Ky@KyNorthstar.me>
2026-01-04 21:17:11 -0700

    Initial Commit

---
+++ HOSTESS Reference Technology/HOSTESS_Reference_TechnologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HOSTESS_Reference_TechnologyApp.swift
+//  HOSTESS Reference Technology
+//
+//  Created by Ky on 2026-01-04.
+//
+
+import SwiftUI
+
+@main
+struct HOSTESS_Reference_TechnologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
